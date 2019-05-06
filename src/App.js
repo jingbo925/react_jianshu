fe96aa1a8f8ad23ac191dbc78c3b6b3aff29b6b7
wangjingbo <jingbo.wang@unicareer.com>
2019-05-06 09:35:07 +0800

    添加首页数据

--- src/App.js
+++ src/App.js
@@ -5,14 +5,25 @@ import { GlobalStyle } from './style';
 import { Iconfont} from './statics/iconfont/iconfont'
 import Header from './common/header'
 import store from './store'
-
+import Home from './pages/home'
+import Detail from './pages/detail'
 function App() {
   return (
     <Provider store={store}>
-     
+      <div>
         <GlobalStyle/>
         <Iconfont/>
         <Header />
+        <BrowserRouter>
+          <div>
+            <Route path='/' exact component={Home}></Route>
+            <Route path='/detail' exact component={Detail} ></Route>
+          </div>
+        </BrowserRouter>
+      </div>
+       
+
+
     </Provider>
   );
 }
---
+++ src/pages/home/store/reducer.js
@@ -0,0 +1,19 @@
+import  {fromJS}  from 'immutable'
+
+const defaultState = fromJS({
+  topicList: [],
+  articleList: [],
+  recommendList: []
+});
+export default (state = defaultState, action) => {
+  switch(action.type) {
+    case 'change_data_home' : 
+      return state.merge({
+        topicList: fromJS(action.topicList),
+        articleList: fromJS(action.articleList),
+        recommendList: fromJS(action.recommendList),
+      })
+    default : 
+      return state;
+  }
+}
